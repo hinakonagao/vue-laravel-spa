f04e8c78ec2060d495e1f665d05b50a8c5d72670
hinako <>
2021-04-02 23:17:06 +0900

    vue.jsとlaravelでtodoを作成

---
+++ routes/api.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get('/tasks', 'App\Http\Controllers\TaskController@index');
+Route::post('/tasks', 'App\Http\Controllers\TaskController@store');
+Route::get('/tasks/{task}', 'App\Http\Controllers\TaskController@show');
+Route::put('/tasks/{task}', 'App\Http\Controllers\TaskController@update');
+Route::delete('/tasks/{task}', 'App\Http\Controllers\TaskController@destroy');
